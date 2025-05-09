7c90c050e92e877155b3f4c0d5d31fb4484b2de1
33301Zp <153008899+33301Zp@users.noreply.github.com>
2025-05-09 14:49:06 -0400

    Delete plant_catalog directory

--- plant_catalog/react-node-express-mongodb-crud/app/config/db.config.js
+++
@@ -1,4 +0,0 @@
-// app/config/db.config.js
-export default {
-    url: process.env.DB_URI,
-};
--- plant_catalog/react-node-express-mongodb-crud/app/models/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import tutorialModel from "./tutorial.model.js"; 
-
-const db = {};
-db.mongoose = mongoose;
-db.url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`; 
-db.tutorials = tutorialModel(mongoose);
-
-export default db;
